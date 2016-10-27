c31042600e9995ebf42bae5511dfb09841fa6742
Lucas Antognoni <antognoni.lucas@gmail.com>
2016-10-27 17:45:24 -0200

    Basic quick sort

--- quick_sort.c
+++ quick_sort.c
@@ -1,3 +1,44 @@
 #include <stdio.h>
 
 #include "quick_sort.h"
+
+void swap(int *vector, int i, int j)
+{
+  int aux;
+
+  aux = vector[i];
+  vector[i] = vector[j];
+  vector[j] = aux;
+}
+
+int partition(int *array, int p, int r)
+{
+  int x, i, j;
+
+  x = array[r];
+  i = p - 1;
+
+  for(j = p; j < r; j++)
+  {
+    if (array[j] <= x)
+    {
+      i = i + 1;
+      swap(array, i, j);
+    }
+  }
+  swap(array, i + 1, j);
+
+  return i + 1;
+}
+
+void quick_sort(int *array, int p, int r)
+{
+  int q;
+
+  if (p < r)
+  {
+    q = partition(array, p, r);
+    quick_sort(array, p, q - 1);
+    quick_sort(array, q + 1, r);
+  }
+}
--- quick_sort.h
+++ quick_sort.h
@@ -1,4 +1,10 @@
 #ifndef QUICK_SORT_H
 #define QUICK_SORT_H
 
+void swap(int *vector, int i, int j);
+
+int partition(int *array, int p, int r);
+
+void quick_sort(int *array, int p, int r);
+
 #endif
