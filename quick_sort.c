ef5fddb79fedf75b1e654d5a900371449ab3d200
Lucas Antognoni <antognoni.lucas@gmail.com>
2016-10-28 16:52:40 -0200

    Quicksort
    Quicksort with different pivot choices

--- quick_sort.c
+++ quick_sort.c
@@ -1,14 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "quick_sort.h"
 
-void swap(int *vector, int i, int j)
+void swap(int *array, int i, int j)
 {
   int aux;
 
-  aux = vector[i];
-  vector[i] = vector[j];
-  vector[j] = aux;
+  aux = array[i];
+  array[i] = array[j];
+  array[j] = aux;
+}
+
+int random_pivot(int *array, int p, int r)
+{
+  int i;
+
+  i = p + rand() % (r - p + 1);
+
+  swap(array, r, i);
+
+  return partition(array, p, r);
+}
+
+int median(int array[])
+{
+  int i = 0, j = 0;
+
+  for (i = 0; i < 4 - 1; i++)
+  {
+    for (j = 4 - 1; j >= i + 1; j--)
+    {
+      if (array[j] < array[j - 1])
+      {
+        swap(array, j, j - 1);
+      }
+    }
+  }
+  return array[1];
+}
+
+int median_pivot(int *array, int p, int r)
+{
+  int m[3];
+
+  m[0] = p + rand() % (r - p + 1);
+  m[1] = p + rand() % (r - p + 1);
+  m[2] = p + rand() % (r - p + 1);
+
+  swap(array, r, median(m));
+
+  return partition(array, p, r);
+}
+
+int middle_pivot(int *array, int p, int r)
+{
+  int i;
+
+  i = (p + r) / 2;
+
+  swap(array, p, i);
+
+  return partition(array, p, r);
 }
 
 int partition(int *array, int p, int r)
@@ -33,10 +87,15 @@ int partition(int *array, int p, int r)
 
 void quick_sort(int *array, int p, int r)
 {
-  int q;
-
   if (p < r)
   {
+    int q;
+
+    //srand((unsigned)time(NULL));
+    //q = random_partition(array, p, r);
+    //q = median_pivot(array, p, r);
+    //q = middle_partition(array, p, r);
+
     q = partition(array, p, r);
     quick_sort(array, p, q - 1);
     quick_sort(array, q + 1, r);
--- quick_sort.h
+++ quick_sort.h
@@ -1,7 +1,15 @@
 #ifndef QUICK_SORT_H
 #define QUICK_SORT_H
 
-void swap(int *vector, int i, int j);
+void swap(int *array, int i, int j);
+
+int random_pivot(int *array, int p, int r);
+
+int median(int array[]);
+
+int median_pivot(int *array, int p, int r);
+
+int middle_pivot(int *array, int p, int r);
 
 int partition(int *array, int p, int r);
 
